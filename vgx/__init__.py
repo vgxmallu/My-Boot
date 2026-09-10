25a676b804c33dbdb540332b0c06b0408b906712
vgxmallu <DeltaAlgor@outlook.com>
2026-09-10 19:03:01 +0530

    Update and rename VgX to __init__.py

---
+++ vgx/__init__.py
@@ -0,0 +1,22 @@
+from pyrogram import Client
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from config import Config
+import logging
+from pyrogram.types import BotCommand
+
+# Setup Logging
+logging.basicConfig(level=logging.INFO)
+# Initialize Scheduler
+scheduler = AsyncIOScheduler()
+
+
+
+
+# Initialize Bot
+app = Client(
+    "scheduler_bot_session",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
+    plugins=dict(root="vgx") # Automatically loads files in plugins/
+)
